a2fc708ba330b54644d56ab53b7eac2191214da6
nicoluarte <nicolas@luarte.net>
2019-10-15 15:33:27 -0300

    added functions to show protocol and display a simple plot, nothing really functional yet

--- chileLogApp/app.R
+++ chileLogApp/app.R
@@ -1,6 +1,7 @@
 library(shiny)
 library(googlesheets)
 library(dplyr)
+library(ggplot2)
 
 ui <- fluidPage(
     # this are the query inputs
@@ -8,7 +9,9 @@ ui <- fluidPage(
     numericInput("cycle", "Ciclo", 0),
     numericInput("pass", "PASSWORD", 0),
     # this is the query table
-    tableOutput("table")
+    tableOutput("table"),
+    tableOutput("table2"),
+    plotOutput("plot1", click = "plot_click")
 )
 
 server <- function(input, output)
@@ -28,6 +31,25 @@ server <- function(input, output)
                                           Numero_ciclo == input$cycle &
                                           Pass == input$pass) %>%
                    select(PASTE))
+    })
+    ## simple plot
+    gsNumbers <- gs %>% gs_read(ws = "Registros")
+    names <- gsNumbers %>% select(Nombre) %>% c()
+    movements <- gsNumbers %>% select(grep("Mov*", names(gsNumbers), value = TRUE)) %>% stack()
+    weights <- gsNumbers %>% select(grep("Peso*", names(gsNumbers), value = TRUE)) %>% stack()
+    toPlot <- data.frame(names = names,
+                         movements = movements,
+                         weights = weights)
+    toPlot[is.na(toPlot)] <- 0
+    #output$table2 <- renderTable({
+    #    data.frame(toPlot %>%
+    #               filter(Nombre == input$name) %>%
+    #               select(weights.values))
+    #})
+    output$plot1 <- renderPlot({
+        y <- toPlot %>% filter(Nombre == input$name) %>%
+            select(weights.values)
+        plot(unlist(y), type = "line")
     })
 }
 
